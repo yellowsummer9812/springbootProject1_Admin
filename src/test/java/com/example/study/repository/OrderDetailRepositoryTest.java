0d586bb6ec44555da5d0198dcbc181edb31af689
yellowsummer <cshee9812@gmail.com>
2021-01-29 09:52:22 +0900

    partner item order_detail 테이블 Test2

--- src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class OrderDetailRepositoryTest extends StudyApplicationTests {
@@ -18,13 +19,17 @@ public class OrderDetailRepositoryTest extends StudyApplicationTests {
 
         OrderDetail orderDetail = new OrderDetail();
 
-        //orderDetail.setOrderAt(LocalDateTime.now());
-
-        // 어떤 사람?
-        //orderDetail.setUserId(4L);
-
+        orderDetail.setStatus("WAITING");
+        // 이틀 뒤에쯤 배송된다
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(900000));
+        // 어떤 장바구니에
+        orderDetail.setOrderGroupId(1L);
         // 어떤 상품?
-        //orderDetail.setItemId(2L);
+        orderDetail.setItemId(1L);
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
 
         OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
         Assertions.assertNotNull(newOrderDetail);
