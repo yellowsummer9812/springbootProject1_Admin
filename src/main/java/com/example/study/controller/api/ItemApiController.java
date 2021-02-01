34da48dff5c4578f1e0d5f4a4b5c0cd7ca37f47e
yellowsummer <cshee9812@gmail.com>
2021-02-01 11:01:35 +0900

    상품 API CREATE

---
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -0,0 +1,41 @@
+package com.example.study.controller.api;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.service.ItemApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
+    @Override
+    @PostMapping("") // api/item
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+        return itemApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}") // api/item/1 ... 100
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("") // api/item
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}") // api/item/1 ... 100
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+}
