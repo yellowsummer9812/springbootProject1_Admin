e6ac677e908469c21bce5f4cb673bbd722033da4
yellowsummer <cshee9812@gmail.com>
2021-02-02 00:28:59 +0900

    주문내역 API DELETE

--- src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -75,7 +75,13 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header delete(Long id) {
-        return null;
+
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> {
+                    orderGroupRepository.delete(orderGroup);
+                    return Header.OK();
+                })
+                .orElseGet(()->Header.ERROR("데이터 없음"));
     }
 
     private Header<OrderGroupApiResponse> response(OrderGroup orderGroup){
