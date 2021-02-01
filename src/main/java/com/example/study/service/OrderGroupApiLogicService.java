622a770d65a791ca72095435edf63bac51eacb85
yellowsummer <cshee9812@gmail.com>
2021-02-02 00:06:20 +0900

    주문내역 API READ

--- src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -41,7 +41,10 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> read(Long id) {
-        return null;
+
+        return orderGroupRepository.findById(id)
+                .map(this::response)
+                .orElseGet(()->Header.ERROR("데이터 없음"));
     }
 
     @Override
